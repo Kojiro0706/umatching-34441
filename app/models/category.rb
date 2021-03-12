7e3baffd476c381875bf2d1066852afc8d546187
Kojiro0706 <buena_vista0706@i.softbank.jp>
2021-03-12 15:41:01 +0900

    categoryモデルの修正

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,8 @@ class Category < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
     { id: 2, name: '雑談' },
-    { id: 3, name: 'マッチング' }
+    { id: 3, name: 'マッチング' },
+    { id: 4, name: '競馬予想' }
   ]
   include ActiveHash::Associations
   has_many :builders
